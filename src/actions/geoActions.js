7f574adde6bec86ce3420cb7967ae311fa02b217
Michaud <michaud@venant.nl>
2017-10-02 23:07:24 +0200

    silly

--- src/actions/geoActions.js
+++ src/actions/geoActions.js
@@ -2,34 +2,39 @@ export const saveStrokeLocation = (club) => {
 
     return (dispatch, getState) => {
 
-        const options = {
-            enableHighAccuracy: true,
-            timeout: 5000,
-            maximumAge: 0
-        };
+        if(navigator && navigator.geolocation) {
+            const options = {
+                enableHighAccuracy: true,
+                timeout: 5000,
+                maximumAge: 0
+            };
 
-        // const success = function (pos) {
-        //     const crd = pos.coords;
+            // const success = function (pos) {
+            //     const crd = pos.coords;
 
-        //     console.log('Your current position is:');
-        //     console.log(`Latitude : ${crd.latitude}`);
-        //     console.log(`Longitude: ${crd.longitude}`);
-        //     console.log(`More or less ${crd.accuracy} meters.`);
-        // };
+            //     console.log('Your current position is:');
+            //     console.log(`Latitude : ${crd.latitude}`);
+            //     console.log(`Longitude: ${crd.longitude}`);
+            //     console.log(`More or less ${crd.accuracy} meters.`);
+            // };
 
-        // const error = function (err) {
-        //     console.warn(`ERROR(${err.code}): ${err.message}`);
-        // };
-        navigator.geolocation.getCurrentPosition((pos) => {
-            const crd = pos.coords;
+            // const error = function (err) {
+            //     console.warn(`ERROR(${err.code}): ${err.message}`);
+            // };
+            navigator.geolocation.getCurrentPosition(function (){}, function (){}, options);
+            navigator.geolocation.getCurrentPosition((pos) => {
+                const crd = pos.coords;
 
-            console.log('Your current position is:');
-            console.log(`Latitude : ${crd.latitude}`);
-            console.log(`Longitude: ${crd.longitude}`);
-            console.log(`More or less ${crd.accuracy} meters.`);
-        }, (err) => {
-            console.warn(`ERROR(${err.code}): ${err.message}`);
-        }, options);//
+                console.log('Your current position is:');
+                console.log(`Latitude : ${crd.latitude}`);
+                console.log(`Longitude: ${crd.longitude}`);
+                console.log(`More or less ${crd.accuracy} meters.`);
+            }, (err) => {
+                console.warn(`ERROR(${err.code}): ${err.message}`);
+            }, options);//
+        } else {
+            console.log('no geo');
+        }
 
     }
 }
