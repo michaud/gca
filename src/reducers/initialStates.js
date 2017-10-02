248323d1886e24b71d1b015c2615c51f815d69d4
Michaud <michaud@venant.nl>
2017-10-02 19:13:33 +0200

    start

---
+++ src/reducers/initialStates.js
@@ -0,0 +1,104 @@
+const coursesData = {
+    name: "coursename",
+    holes: [],
+    slope: undefined
+};
+
+const gameHolesData = [{
+    id: 0,
+    name: "hole 1",
+    par: 4,
+    strokeIndex: 9,
+    teeLocation: undefined,
+    holeLocation: undefined
+}];
+
+const gamesData = [{
+    name: undefined,
+    gameDate: undefined,
+    bag: bagData,
+    playingHandicap: undefined,
+    handicap: undefined,
+    players: {
+        player: {
+            name: undefined,
+            exactHandicap: undefined
+        },
+        marker: {
+            name: undefined,
+            exactHandicap: undefined
+        }
+    },
+    course: {
+        name: "coursename",
+        holes: [],
+        slope: undefined
+    }
+}];
+
+const bagData = {
+    clubs:clubData
+};
+
+const clubData = [{
+    name: "putter",
+    brand: "ping"
+},
+{
+    name: "Lob wedge",
+    brand: "ping"
+},
+{
+    name: "Sand wedge",
+    brand: "ping"
+},
+{
+    name: "Pitching wedge",
+    brand: "ping"
+},
+{
+    name: "9 iron",
+    brand: "ping"
+},
+{
+    name: "8 iron",
+    brand: "ping"
+},
+{
+    name: "7 iron",
+    brand: "ping"
+},
+{
+    name: "6 iron",
+    brand: "ping"
+},
+{
+    name: "5 iron",
+    brand: "ping"
+},
+{
+    name: "4 iron",
+    brand: "ping"
+},
+{
+    name: "5 wood",
+    brand: "ping"
+},
+{
+    name: "rescue wood",
+    brand: "ping"
+},
+{
+    name: "3 wood",
+    brand: "ping"
+},
+{
+    name: "Driver",
+    brand: "ping"
+}];
+
+export const INITIAL_CLUBS_STATE = clubData;
+export const INITIAL_GAME_HOLES_STATE = gameHolesData;
+export const INITIAL_GAMES_STATE = gamesData;
+export const INITIAL_COURSES_STATE = coursesData;
+export const INITIAL_BAG_STATE = bagData;
