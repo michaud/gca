f937e498531e8eaabd4186584de0258e3a98a052
Michaud <michaud@venant.nl>
2017-10-03 15:39:33 +0200

    demo

--- src/components/map/StrokesMap.jsx
+++ src/components/map/StrokesMap.jsx
@@ -4,7 +4,7 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker, Circle } from "react-go
 
 const StrokesMap = compose(
     withProps({
-        googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyANxegeLo_lxopwKIgjVfvZsPwwX7T7qbE&v=3.exp&libraries=geometry,drawing,places",
         loadingElement: <div style={{ height: `100%` }} />,
         containerElement: <div style={{ height: `400px` }} />,
         mapElement: <div style={{ height: `100%` }} />,
