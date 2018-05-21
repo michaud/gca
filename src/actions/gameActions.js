a2c9d0ff69fc29464369146ac8d12c4256a8282e
Michaud <michaud@venant.nl>
2018-05-21 20:46:28 +0200

    Updates

--- src/actions/gameActions.js
+++
@@ -1,6 +0,0 @@
-import { START_GAME } from 'actions/actionTypes';
-
-export const startGame = (gameData) => ({
-    type: START_GAME,
-    gameData
-});
--- src/actions/markerActions.js
+++
@@ -1,6 +0,0 @@
-import { ADD_MARKER } from 'actions/actionTypes';
-
-export const addMarker = (marker) => ({
-    type: ADD_MARKER,
-    marker
-});
